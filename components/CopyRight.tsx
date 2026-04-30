dd40b145c58953371ecdd1c4f850003e860758c9
Ekeoma Agu <emanixworld@gmail.com>
2026-04-30 09:27:39 +0100

    added a copyright component

---
+++ components/CopyRight.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CopyRight = () => {
+  return (
+    <>
+        <div className="mt-12 text-center text-sm text-gray-500">
+          {'© '} {new Date().getFullYear()} SAV Team - Directorate of Academic Planning
+        </div>
+    </>
+  )
+}
+
+export default CopyRight
